af1b9c90a6e6541821d014706480aad2005816e3
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2025-05-31 14:19:49 +0000

    Add calculate ema

--- src/calculations.ts
+++ src/calculations.ts
@@ -1,8 +1,22 @@
 /*
 For ema 21 day window is given
+In ema more weight is given to the recent price
 
+ema[i] = (data[i] * (smoothing/1 + 21)) + (ema[i-1] * (1 - (smoothing/1 + 21))
+this is the formula for ema 
+smoothing is by default considered 2
 */
 
+export function calculateEMA(data: number[], period: number): number[] {
+  const weight = 2 / (1 + period);
+  const ema: number[] = [];
+  ema[0] = data[0];
+  for (let i = 1; i < data.length; i++) {
+    ema[i] = data[i] * weight + ema[i - 1] * (1 - weight);
+  }
+
+  return ema;
+}
 
 /*
 For RSI a 14 day window is considered
